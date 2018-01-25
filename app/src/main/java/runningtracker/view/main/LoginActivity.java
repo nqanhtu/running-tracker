20aeb3b7d497321f796cd7797a5234d2ea36239d
Nguyễn Quốc Anh Tú <ngqanhtu@outlook.com>
2018-01-25 20:43:09 +0700

    add login activity

---
+++ app/src/main/java/runningtracker/view/main/LoginActivity.java
@@ -0,0 +1,25 @@
+package runningtracker.view.main;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import runningtracker.R;
+import runningtracker.view.running.MainActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        // TODO Use fields...
+    }
+    @OnClick(R.id.buttonHome)
+    public void startHomeActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivityHome.class);
+        startActivity(intent);
+    }
+}
