2e919bcb7546c77d426119a1a301ac5f6974e434
Tu Nguyen <ngqanhtu@outlook.com>
2018-05-10 21:06:33 +0700

    change bottom bar

--- app/src/main/java/runningtracker/home/HomeActivity.java
+++ app/src/main/java/runningtracker/home/HomeActivity.java
@@ -3,11 +3,15 @@ package runningtracker.home;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import runningtracker.R;
 import runningtracker.dashboard.DashboardFragment;
@@ -15,9 +19,10 @@ import runningtracker.notification.NotificationsFragment;
 import runningtracker.profile.ProfileFragment;
 import runningtracker.running.RunningActivity;
 
-public class HomeActivity extends AppCompatActivity implements  DashboardFragment.OnFragmentInteractionListener{
+public class HomeActivity extends AppCompatActivity implements DashboardFragment.OnFragmentInteractionListener {
 
-    AHBottomNavigation bottomNavigation;
+    @BindView(R.id.navigation)
+    BottomNavigationView bottomNavigation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,53 +30,36 @@ public class HomeActivity extends AppCompatActivity implements  DashboardFragmen
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
 
-        bottomNavigation= (AHBottomNavigation) findViewById(R.id.navigation);
-        createNavItems();
-
 
         // Default fragment
-        DashboardFragment dashboardFragment =new DashboardFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_container,dashboardFragment).commit();
+        DashboardFragment dashboardFragment = new DashboardFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, dashboardFragment).commit();
 
-        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onTabSelected(int position, boolean wasSelected) {
-                if (position==0)
-                {
-                    DashboardFragment dashboardFragment =new DashboardFragment();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_container,dashboardFragment).commit();
-                }else  if (position==1)
-                {
-                    ProfileFragment profileFragment =new ProfileFragment();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_container,profileFragment).commit();
-                }else  if (position==2)
-                {
-                    NotificationsFragment notificationsFragment =new NotificationsFragment();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_container,notificationsFragment).commit();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.navigation_dashboard:
+                        DashboardFragment dashboardFragment = new DashboardFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, dashboardFragment).commit();
+                        return true;
+                    case R.id.navigation_profile:
+                        ProfileFragment profileFragment = new ProfileFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, profileFragment).commit();
+                        return true;
+                    case R.id.navigation_notifications:
+                        NotificationsFragment notificationsFragment = new NotificationsFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, notificationsFragment).commit();
+                        return true;
                 }
-                return true;
+                return false;
+
             }
         });
-    }
-
-    private void createNavItems()
-    {
-        // Create items
-        AHBottomNavigationItem item1 = new AHBottomNavigationItem("Dashboard", R.drawable.ic_dashboard_black_24dp);
-        AHBottomNavigationItem item2 = new AHBottomNavigationItem("Profile", R.drawable.ic_profile_24dp);
-        AHBottomNavigationItem item3 = new AHBottomNavigationItem("Notifications", R.drawable.ic_notifications_black_24dp);
-        // Add items
-        bottomNavigation.addItem(item1);
-        bottomNavigation.addItem(item2);
-        bottomNavigation.addItem(item3);
-
-        //set properties
-        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#FEFEFE"));
-
-        //set current item
-        bottomNavigation.setCurrentItem(0);
 
     }
+
 
     @Override
     public void onStartRunning() {
